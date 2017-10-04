2daebb5e797c5f759c51910e0b70f1d236705362
Shriram Bhosle <sb43@sanger.ac.uk>
2017-10-04 14:41:39 +0100

    removed stale files

--- cgp_downsampler/.bpipe/commandtmp/29/cmd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -exu
-   set -o pipefail
-(SAMPLE=$(awk '(NR==2) {print $2}' ../../downsample_input/CAL-27_E23.bam.bas) &&          mkdir -p ~/sb43_scratch116/rt_user/downsampling_564469/scripts/cgp_downsampler/downsampledBams/$SAMPLE && 				 bwa_mem.pl -b '-T 30 -Y' -mt 6 -o ~/sb43_scratch116/rt_user/downsampling_564469/scripts/cgp_downsampler/downsampledBams/$SAMPLE -r /lustre/scratch112/sanger/cgppipe/canpipe/live/ref/human/GRCh37d5/genome.fa -t 6  -s $SAMPLE ~/sb43_scratch116/rt_user/downsampling_564469/scripts/cgp_downsampler/downsampledBams/$SAMPLE.bam &&  				 rm ~/sb43_scratch116/rt_user/downsampling_564469/scripts/cgp_downsampler/downsampledBams/$SAMPLE.bam &&  				 date >CAL-27_E23.bwa_mem.track) > .bpipe/commandtmp/29/cmd.out
--- cgp_downsampler/.bpipe/commandtmp/9/cmd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -exu
-   set -o pipefail
-(mkdir -p ~/sb43_scratch116/rt_user/downsampling_564469/scripts/cgp_downsampler/downsampledBams &&             DS_FACTOR=$( awk -v bases=3137454505 '($0!~/#/) {sum=sum+$9} END { print (17+1)/(sum/bases)}' ../../downsample_input/CAL-27_E16.A20sH12qE11.bam.bas) && 					 SAMPLE=$(awk '(NR==2) {print $2}' ../../downsample_input/CAL-27_E16.A20sH12qE11.bam.bas) && 					 /software/CGP/canpipe/test/bin/canpipe_test bamdownsamplerandom seed=3137454505 p=$DS_FACTOR I=../../downsample_input/CAL-27_E16.A20sH12qE11.bam level=1 O=~/sb43_scratch116/rt_user/downsampling_564469/scripts/cgp_downsampler/downsampledBams/$SAMPLE.bam &&  					 date >CAL-27_E16.A20sH12qE11.do_downsampling.track) > .bpipe/commandtmp/9/cmd.out
